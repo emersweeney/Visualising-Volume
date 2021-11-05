66d4bd67be9460ea5f91d933e9de88ecdaac0d04
emersweeney <82947228+emersweeney@users.noreply.github.com>
2021-11-05 13:26:11 +0000

    LevelMenu
    Added Level Menu template

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void ReplayLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+    }
+
+    public void NextLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
